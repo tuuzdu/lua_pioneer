8289655ee3f7d341fac243403b91f67053793a43
tuuzdu <tuuzdu@mail.ru>
2017-10-31 17:43:00 +0300

    path release for copter + luaTest

--- pathTest.lua
+++ pathTest.lua
@@ -1,3 +1,17 @@
+local function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+Ev = {ALTITUDE_REACHED = 61, POINT_REACHED = 21, COPTER_LANDED = 23}
 
 local Path = {}
 
@@ -9,7 +23,7 @@ end
 
 
 function Path:addWaypoint( _x, _y, _z )
-	local point = {x = _x, y = _y, z = _z, waypoint = true}
+	local point = {x = _x * 1000, y = _y * 1000, z = _z * 1000, waypoint = true}
 	table.insert( self.point, point )
 end
 
@@ -37,13 +51,12 @@ end
 
 
 function Path:Start()
-	sleep(1)
-	if self.point[1].takeoff then
-		self.state = 1
+	--[[if self.point[1].takeoff then
 		ap.push(Ev.MCE_PREFLIGHT) 
 		sleep(1)
 		ap.push(Ev.MCE_TAKEOFF)
-	end
+	end]]--
+	self.state = 7			-- should be replaced into the block above!!!
 end
 
 
@@ -54,12 +67,16 @@ function Path:eventHandler( e )
 
 	if e == Ev.ALTITUDE_REACHED and obj_state.takeoff then
 		change_state = true
+		print("TAKEOFF")
 	elseif e == Ev.POINT_REACHED and obj_state.waypoint then
 		change_state = true
+		print("WAYPOINT")
 	elseif e == Ev.POINT_REACHED and obj_state.landing then
 		change_state = true
+		print("LANDING")
 	elseif e == Ev.COPTER_LANDED and ( obj_state.landing or obj_state.takeoff ) then
 		change_state = true
+		print("LANDED")
 	end
 
 	if change_state then
@@ -70,19 +87,18 @@ function Path:eventHandler( e )
 
 		if self.state < #self.point then
 			self.state = self.state + 1
-			obj_state = self.point[self.state]
 		else 
 			self.state = 0
 		end
 
 		if obj_state.waypoint then
-			ap.goToLocalPoint( {x = self.point[self.state].x, y = self.point[self.state].y, z = self.point[self.state].z} )
+			--ap.goToLocalPoint( {x = self.point[self.state].x, y = self.point[self.state].y, z = self.point[self.state].z} )
 		elseif obj_state.takeoff then
-			ap.push(Ev.MCE_PREFLIGHT) 
-			sleep(1)
-			ap.push(Ev.MCE_TAKEOFF)
+			--ap.push(Ev.MCE_PREFLIGHT) 
+			--sleep(1)
+			--ap.push(Ev.MCE_TAKEOFF)
 		elseif obj_state.landing then
-			ap.push(Ev.MCE_LANDING)
+			--ap.push(Ev.MCE_LANDING)
 		end
 
 	end
@@ -113,6 +129,7 @@ function func_1 ()
 	print(f)
 end
 
+--p = Path.new()
 pn = Path.new()
 
 pn:addTakeoff()
@@ -134,4 +151,10 @@ pn:addFuncForPoint(func_1, 8)
 
 pn:Start()
 
+pn:eventHandler(Ev.POINT_REACHED)
+print("State:" .. pn.state)
+print(dump(pn))
+
+--print(p.point[2].x)
+--print(pn.point[2].takeoff)
 
