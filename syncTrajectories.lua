d018dc29c797dfa2b0784a2692581c05c2c7aac2
tuuzdu <tuuzdu@mail.ru>
2017-11-01 14:53:20 +0300

    Trajectories init format

--- syncTrajectories.lua
+++ syncTrajectories.lua
@@ -1,115 +1,88 @@
+local function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+
 local boardNumber = boardNumber
 
-local delta = TimeInfo.new("TimeDelta")
-local launch = TimeInfo.new("LaunchTime")
+--local delta = TimeInfo.new("TimeDelta")
+--local launch = TimeInfo.new("LaunchTime")
 
-local led_count = 4
-local leds = Ledbar.new(led_count)
-
-local curr_state = "START"
-
-local ledStateRed = false
-local ledStateGreen = false
-local ledStateBlue = false
-
-local time_transition = 8
-
-local current_point = 0
-local old_point = 1 
-local start = false
-local time_delta = 0
-local time_start_global = 0
-
-local point =  { 
-		    	{{x=1000, y=0, z=1100}, {x=0, y=1000, z=1100}, {x=-1000, y=0, z=1100}, {x=0, y=-1000, z=1100}, {x=250, y=-250, z=1100}},
-			{{x=0, y=1000, z=1100}, {x=-1000, y=0, z=1100}, {x=0, y=-1000, z=1100}, {x=1000, y=0, z=1100}, {x=250, y=250, z=1100}},			
-			{{x=-1000, y=0, z=1100}, {x=0, y=-1000, z=1100}, {x=1000, y=0, z=1100}, {x=0, y=1000, z=1100}, {x=-250, y=250, z=1100}}
-			
-		}
+--local led_count = 4
+--local leds = Ledbar.new(led_count)
 
 
-
-
-leds:set(0, {r=1, g=0.5, b=0})
-leds:set(3, {r=1, g=0.5, b=0})
-
-
-
-if boardNumber < 1 then
-	boardNumber = 1
-end
--- заменить 3 на длинну массива
-if boardNumber > 3 then
-	boardNumber = 3
-end
-
-
-
-local function allLED(red,green,blue)
-		for i = 0, led_count - 1, 1 do
-        		leds:set(i, {r=red, g=green, b=blue})
-      		end
-end
-
-local function LEDRed()
-	if ledStateRed then
-		ledStateRed = false
-		leds:set(0, {r=0, g=0 , b=0})
-	else 
-		ledStateRed = true
-		leds:set(0, {r=1, g=0 , b=0})
-	end 
-end
-
-local function LEDGreen()
-	if ledStateGreen then
-		ledStateGreen = false
-		leds:set(3, {r=0, g=0 , b=0})
-	else 
-		ledStateGreen = true
-		leds:set(3, {r=0, g=1 , b=0})
-	end 
-end
-
-local function LEDBlue()
-	if ledStateBlue then
-		ledStateBlue = false
-		leds:set(1, {r=0, g=0 , b=0})
-		leds:set(2, {r=0, g=0 , b=0})
-	else 
-		ledStateBlue = true
-		leds:set(1, {r=0, g=0 , b=1})
-		leds:set(2, {r=0, g=0 , b=1})
-	end 
-end
-
-
-local function get_global_time()	
-	return time() +  delta:retrieve()
-end
-
-action = {
-	["START"] = function(x)
-		leds:set(1, {r=1, g=0, b=1})
-		leds:set(2, {r=1, g=0, b=1})
-		ap.push(Ev.MCE_PREFLIGHT)
-		sleep(2)
-		ap.push(Ev.MCE_TAKEOFF)
-		time_start_global = launch:retrieve()
-		-- переход в следующее состояние 
-		curr_state = "LANDING"
-	end,
-	["LANDING"] = function (x)
- 		ap.push(Ev.MCE_LANDING)
-		curr_state = "START"
-	end
-	
+-- Trajectories[a][b][c], где a - номер формации, b - номер борта, с - соответственно x, y, z, A, B
+-- Trajectories.initial_pose[b][c], где b - номер борта, с - соответственно x, y, z
+Trajectories = {
+	initial_pose = { 
+		{-0.6, 0.3, 0},
+		{0.6, 0.3, 0},
+		{-0.6, -0.3, 0},
+		{0.6, -0.3, 0}
+	},
+	{ 
+		xy_first = false, 
+		{0.00666667, 0.6, 0.6, 0, 0},
+		{0.3, 0.2, 0.2, 0, 0},
+		{-0.3, 0.2, 0.2, 0, 0},
+		{0.0133333, -0.2, -0.2, 0, 0}
+	},
+	{ 
+		xy_first = true, 
+		{-0.6, 0.3, 0, 0, 0},
+		{0.6, 0.3, 0, 0, 0},
+		{-0.6, -0.3, 0, 0, 0},
+		{0.6, -0.3, 0, 0, 0}
+	},
+	{ 
+		xy_first = false, 
+		{0.00666667, 0.2, 0.2, 0, 0.23284},
+		{0.00666667, 0.6, 0.6, 0, -0.140381},
+		{-0.00666667, -0.6, -0.6, 0, 0.313945},
+		{0.00666667, -0.2, -0.2, 0, 0}
+	},
+	{ 
+		xy_first = true, 
+		{-0.6, 0.3, 0, 0, 0.23284},
+		{0.6, 0.3, 0, 0, -0.140381},
+		{-0.6, -0.3, 0, 0, 0.313945},
+		{0.6, -0.3, 0, 0, 0}
+	},
+	{ 
+		xy_first = false, 
+		{-0.293333, 0.6, 0.6, 0, 0},
+		{0.306667, 0.2, 0.2, 0, 0},
+		{-0.3, -0.2, -0.2, 0, 0},
+		{0.313333, -0.6, -0.6, 0, 0}
+	},
+	{ 
+		xy_first = true, 
+		{-0.6, 0.3, 0, 0, 0},
+		{0.6, 0.3, 0, 0, 0},
+		{-0.6, -0.3, 0, 0, 0},
+		{0.6, -0.3, 0, 0, 0}
+	}
 }
+
+
+--print(dump(Trajectories))
+print(Trajectories[1][2][1])
+print(Trajectories.initial_pose[1][1])
 
 
 
 function callback(event)
-
+--[[
 	if (event == Ev.SHOCK) then
 		allLED(1,0,0)
 	end
@@ -136,10 +109,12 @@ function callback(event)
 		ap.updateYaw(0.001, 0)
 		start = true
 	end
+]]--
 end
 
 
 function loop() 
+--[[
 	if start then
 		local time_from_start = math.abs (get_global_time() - time_start_global)
 		current_point = math.floor (time_from_start / time_transition)  
@@ -152,6 +127,7 @@ function loop()
 		
 		end
 	end
-
+]]--
 
 end
+
