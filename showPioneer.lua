e5f1f2c42c22be9a89f5733b5d538437993b63a2
tuuzdu <tuuzdu@mail.ru>
2018-03-12 19:21:34 +0300

    Show with lettering "Russia"

--- showPioneer.lua
+++ showPioneer.lua
@@ -1,4 +1,3 @@
-
 local Path = {}
 
 -- Создает новый объект класса Path
@@ -9,8 +8,8 @@ function Path.new()
 end
 
 -- Добавляет точку пути. Аргументы: x, y, z - координаты в метрах; func - ссылка на функцию, выполняемую после достижения точки (необязательный аргумент).
-function Path:addWaypoint( _x, _y, _z, _func )
-	local point = { x = _x, y = _y, z = _z, waypoint = true }
+function Path:addWaypoint( _x, _y, _z, _func, _bound )
+	local point = { x = _x, y = _y, z = _z, waypoint = true, bound = _bound }
 	table.insert( self.point, point )
 	if _func then
 		self.point[#self.point].func = _func
@@ -35,58 +34,54 @@ end
 
 -- Запуск выполнения полетного задания
 function Path:start()
-	sleep(1)
+	--sleep(1)
+	localSleep(1)
 	if self.point[1].takeoff then
 		self.state = 1
 		ap.push(Ev.MCE_PREFLIGHT) 
-		sleep(1)
+		--sleep(1)
+		localSleep(1)
 		ap.push(Ev.MCE_TAKEOFF)
 	end
 end
 
 -- Обработчик событий объекта пути. Должен быть добавлен в function callback(event) с передачей аргумента event. Как в примере ниже.
 function Path:eventHandler( e )
-
 	local change_state = false
 	local obj_state = self.point[self.state]
-
 	if e == Ev.ALTITUDE_REACHED and obj_state.takeoff then
 		change_state = true
-	elseif e == Ev.POINT_REACHED and obj_state.waypoint then
+	elseif e == Ev.POINT_DECELERATION and obj_state.waypoint and not obj_state.bound then
 		change_state = true
-	elseif e == Ev.POINT_REACHED and obj_state.landing then
+	elseif e == Ev.POINT_REACHED and obj_state.waypoint and obj_state.bound then
+		change_state = true
+	elseif e == Ev.POINT_DECELERATION and obj_state.landing then
 		change_state = true
 	elseif e == Ev.COPTER_LANDED and ( obj_state.landing or obj_state.takeoff ) then
 		change_state = true
 	end
-
 	if change_state then
-
 		if obj_state.func then 
 			obj_state.func()
 		end
-
 		if self.state < #self.point then
 			self.state = self.state + 1
 			obj_state = self.point[self.state]
 		else 
 			self.state = 0
 		end
-
 		if obj_state.waypoint then
 			ap.goToLocalPoint( {x = self.point[self.state].x, y = self.point[self.state].y, z = self.point[self.state].z} )
 		elseif obj_state.takeoff then
 			ap.push(Ev.MCE_PREFLIGHT) 
-			sleep(1)
+			--sleep(1)
+			localSleep(1)
 			ap.push(Ev.MCE_TAKEOFF)
 		elseif obj_state.landing then
 			ap.push(Ev.MCE_LANDING)
 		end
-
 	end
 end
-
-
 
 -- Управление светодиодами
 local led_count = 29
@@ -101,12 +96,14 @@ local colors = {	purple = 	{r=1, g=0, b=1},
 					green = 	{r=0, g=1, b=0}, 
 					white = 	{r=1, g=1, b=1}, 
 					black = 	{r=0, g=0, b=0}	}
+
 -- Управление светодиодами на плате
 local function setSysLeds( color )
 	for i = 0, led_offset - 1, 1 do
 		leds:set(i, color)
 	end
 end
+
 -- Возвращает указатель на функцию включения матрицы заданным цветом
 local function getPointerLedsMatrix( color )
 	return function()
@@ -115,77 +112,127 @@ local function getPointerLedsMatrix( color )
 		end
 	end
 end
+
 -- Таблица с указателями 
 local pColors = {}
 for k, v in pairs(colors) do 
 	pColors[k] = getPointerLedsMatrix(v)
 end
 
+local delta = TimeInfo.new("TimeDelta")
+local launch = TimeInfo.new("LaunchTime")
 
+function localSleep( wait_time )
+	last_time = time()
+	while (time() - last_time < wait_time) do
+	end
+end
+
+local function getGlobalTime()	
+	return time() +  delta:retrieve()
+end
+
+local function getPointerSleep( time )
+	return function()
+		--sleep (time)
+		localSleep(time)
+	end
+end
+
+local function endWaiting( wait_time )
+	return function()
+		while (getGlobalTime() - launch:retrieve() < wait_time) do
+		end
+	end
+end
 
 -- Инициализация вспомогательных функций
 local board_number = boardNumber
 local start_formations = false
--- local start_init_pose = false
--- local time_delta = 0
--- local time_start_global = 0
--- local time_transition = 5
-
 local unpack = table.unpack	
 
-local delta = TimeInfo.new("TimeDelta")
-local launch = TimeInfo.new("LaunchTime")
+local endWaitingTime = 80
 -- Таблица с координатами и цветом
 local letters = {
-	{	-- 1 "П"
-		{-2, -1, 0.5, pColors.blue},
-		{-2, -1, 1.5, pColors.blue},
-		{-1.5, -1, 1.5, pColors.blue},
-		{-1.5, -1, 0.5, pColors.black}
+	{	-- 1 "Р"
+		{-2, 0.5, 0.5, pColors.black, true},
+		{-2, -1, 0.5, pColors.white, true},
+		{-2, -1, 1.5, pColors.white, false},
+		{-1.6, -1, 1.5, pColors.white, false},
+		{-1.5, -1, 1.4, pColors.white, false},
+		{-1.5, -1, 0.9, pColors.white, false},
+		{-1.6, -1, 0.8, pColors.white, false},
+		{-2, -1, 0.8, pColors.black, true},
+		{-2, 0.5, 0.5, endWaiting(endWaitingTime), true}
+	},
+	{	-- 2 "О"
+		{-1.3, 1.5, 0.5, getPointerSleep(10), true},
+		{-1.3, -1, 0.6, pColors.white, true},
+		{-1.3, -1, 1.4, pColors.white, false},
+		{-1.2, -1, 1.5, pColors.white, false},
+		{-0.9, -1, 1.5, pColors.white, false},
+		{-0.8, -1, 1.4, pColors.white, false},
+		{-0.8, -1, 0.6, pColors.white, false},
+		{-0.9, -1, 0.5, pColors.white, false},
+		{-1.2, -1, 0.5, pColors.white, false},
+		{-1.3, -1, 0.6, pColors.black, true},
+		{-1.3, 1.5, 0.5, endWaiting(endWaitingTime), true}
+	},
+	{	-- 2 "С1"
+		{-0.6, 0.5, 0.5, getPointerSleep(17), true},
+		{-0.1, -1, 1.4, pColors.blue, true},
+		{-0.2, -1, 1.5, pColors.blue, false},
+		{-0.5, -1, 1.5, pColors.blue, false},
+		{-0.6, -1, 1.4, pColors.blue, false},
+		{-0.6, -1, 0.6, pColors.blue, false},
+		{-0.5, -1, 0.5, pColors.blue, false},
+		{-0.2, -1, 0.5, pColors.blue, false},
+		{-0.1, -1, 0.6, pColors.black, true},
+		{-0.6, -1, 0.6, pColors.black, false},
+		{-0.6, 0.5, 0.5, endWaiting(endWaitingTime), true}
+	},
+	{	-- 3 "C2"
+		{0.1, 1.5, 0.5, getPointerSleep(24), true},
+		{0.6, -1, 1.4, pColors.blue, true},
+		{0.5, -1, 1.5, pColors.blue, false},
+		{0.2, -1, 1.5, pColors.blue, false},
+		{0.1, -1, 1.4, pColors.blue, false},
+		{0.1, -1, 0.6, pColors.blue, false},
+		{0.2, -1, 0.5, pColors.blue, false},
+		{0.5, -1, 0.5, pColors.blue, false},
+		{0.6, -1, 0.6, pColors.black, true},
+		{0.1, -1, 0.6, pColors.black, false},
+		{0.1, 1.5, 0.5, endWaiting(endWaitingTime), true}
 	},
 	{	-- 2 "И"
-		{-1.3, 0, 1.5, pColors.red},
-		{-1.3, 0, 0.5, pColors.red},
-		{-0.8, 0, 1.5, pColors.red},
-		{-0.8, 0, 0.5, pColors.black}
+		{0.8, 0.5, 0.5, getPointerSleep(32), true},
+		{1.3, -1, 0.5, pColors.red, true},
+		{1.3, -1, 1.5, pColors.red, false},
+		{0.8, -1, 0.5, pColors.red, true},
+		{0.8, -1, 1.5, pColors.black, true},
+		{0.8, 0.5, 0.5, endWaiting(endWaitingTime), true}
 	},
-	{	-- 3 "О"
-		{-0.6, 1, 0.5, pColors.green},
-		{-0.6, 1, 1.5, pColors.green},
-		{-0.1, 1, 1.5, pColors.green},
-		{-0.1, 1, 0.5, pColors.green},
-		{-0.6, 1, 0.5, pColors.black}
-	},
-	{	-- 4 "Н"
-		{0.1, 1, 0.5, pColors.purple},
-		{0.1, 1, 1.5, pColors.black},
-		{0.6, 1, 1.5, pColors.purple},
-		{0.6, 1, 0.5, pColors.black},
-		{0.6, 1, 0.8, pColors.purple},
-		{0.1, 1, 0.8, pColors.black}
-	},
-	{	-- 5 "Е"
-		{1.3, 0, 0.5, pColors.cyan},
-		{0.8, 0, 0.5, pColors.cyan},
-		{0.8, 0, 1.5, pColors.cyan},
-		{1.3, 0, 1.5, pColors.black},
-		{1.1, 0, 0.8, pColors.cyan},
-		{0.8, 0, 0.8, pColors.black}
-	},
-	{	-- 6 "Р"
-		{1.5, -1, 0.5, pColors.yellow},
-		{1.5, -1, 1.5, pColors.yellow},
-		{2, -1, 1.5, pColors.yellow},
-		{2, -1, 0.8, pColors.yellow},
-		{1.5, -1, 0.8, pColors.black}
+	{	-- 6 "Я"
+		{1.5, 1.5, 0.5, getPointerSleep(40), true},
+		{2.0, -1, 0.5, pColors.red, true},
+		{2.0, -1, 1.5, pColors.red, false},
+		{1.6, -1, 1.5, pColors.red, false},
+		{1.5, -1, 1.4, pColors.red, false},
+		{1.5, -1, 0.8, pColors.red, false},
+		{1.6, -1, 0.8, pColors.red, false},
+		{2.0, -1, 0.8, pColors.red, true},
+		{1.5, -1, 0.5, pColors.black, true},		
+		{1.5, 1.5, 0.5, pColors.black, true},
+		{1.5, 1.5, 0.5, endWaiting(endWaitingTime), true}
 	}
 }
 
 function callback ( event )
-
 	if (event == Ev.SYNC_START) then
-		-- setSysLeds(colors.cyan)
-
+		setSysLeds(colors.cyan)
+		--sleep (2)
+		localSleep(2)
+		setSysLeds(colors.black)
 		if start_formations then
 			start_formations = false
 			ap.push(Ev.MCE_LANDING)
@@ -205,12 +252,10 @@ end
 function loop()
 end
 
-
-
 -- Создание Path для коптера по номеру борта
 letter_path = Path.new()
 letter_path:addTakeoff()
 for _, v in ipairs(letters[board_number]) do
 	letter_path:addWaypoint(unpack(v))
 end
-letter_path:addLanding()
+letter_path:addLanding(function () start_formations = false end)
